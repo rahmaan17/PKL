b7ebe40f36b8cd8a44a9b1b3b1b845db57e17c20
rahmaan17 <55658175+rahmaan17@users.noreply.github.com>
2020-01-18 16:15:58 +0700

    Update evaluasi-2.js

--- evaluasi-sprint-1/evaluasi-2.js
+++ evaluasi-sprint-1/evaluasi-2.js
@@ -1,18 +1,3 @@
-// var n = 9;
-
-// for(i=1; i<=n; i++){
-//     for(j=1;j<=n; j++){
-//         if(i%2==1 && j%2==1 || i%2 ==0 && j%2==0){
-//             document.write("+ ");
-//         }
-//         else{
-//             document.write("- ");
-//         }
-//     }
-//     document.write("<br>");    
-// }
-
-
 var n = 9;
 
 for(i=1; i<= n; i++){
